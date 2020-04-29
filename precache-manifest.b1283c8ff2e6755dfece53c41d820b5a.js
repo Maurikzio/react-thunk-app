b20426a001e869b94d09dd827b183a56977fd0a7
Maurikzio <m.morocho1196@gmail.com>
2020-04-29 09:07:17 +0300

    Updates

---
+++ precache-manifest.b1283c8ff2e6755dfece53c41d820b5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca66ddd7f350e0204900d2f459bc1ee8",
+    "url": "/react-thunk-app/index.html"
+  },
+  {
+    "revision": "b3b4872b160441129baa",
+    "url": "/react-thunk-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0aa11817f608f60ed09a",
+    "url": "/react-thunk-app/static/js/2.22341c0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-thunk-app/static/js/2.22341c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b4872b160441129baa",
+    "url": "/react-thunk-app/static/js/main.8b89cea8.chunk.js"
+  },
+  {
+    "revision": "b037bf9d5ce94c93b2c9",
+    "url": "/react-thunk-app/static/js/runtime-main.7a7b7f70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-thunk-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
